ca3a997bdbb79ba254dbb2f300d320d99c602380
Maitry <maitrypaunikar.27@gmail.com>
2022-05-09 18:10:42 +0530

    home file change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    Dashboard
+                    Home Dashboard
                 </div>
 
                 <div class="card-body">
